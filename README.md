eeb51fe889a6d6fbd196d945553105113a93fb5f
“bobleesj” <bobleesj@gmail.com>
2023-02-13 17:30:50 -0500

    Updated the to-do for this week

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Back-end for CooperExchange
 
 Pending Tasks
-- [ ] Use Java to update username, email, password, first name, last name
+- [ ] Run a Java server using SpringBoot
+- [ ] Implement Docker workflow
+- [ ] Handle account registration and login requesets
 - [ ] Refactor code using `DataAccessObject` and `DataTransferObject`
 
 Implemented
-- [x] Use Java to create an account
-- [x] Use Java to connect with the local database and retrieve information based on `user_id`
+- [x] Use Java to update a property value using `user_id` - 02/13/2023
+- [x] Use Java to create an account - 02/13/2023
+- [x] Use Java to connect with the local database and retrieve information based on `user_id` 0 02/13/2023
 
 ## How to use
 
--- src/main/java/cooperexchange/App.java
+++ src/main/java/cooperexchange/App.java
@@ -1,13 +1,17 @@
+
 package cooperexchange;
 
 import cooperexchange.Account;
 import java.sql.*;
 import java.util.Random;
+
+
 public class App {
     private Connection connection;
     public static void main(String args[]) {
 
         try {
+            // Connect with the database
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager
                     .getConnection("jdbc:postgresql://localhost:5432/cooper_exchange",
@@ -16,14 +20,11 @@ public class App {
             System.out.println("Successfully connected to the local database");
 
             Account account = new Account("bobleesj", "abc123");
-            int upperbound = 1000;
-            // Generating random values from 0 - 24
-            // using nextInt()
-            Random rand = new Random();
-            int int_random = rand.nextInt(upperbound);
 
             // INSERT: Create an account
-
+            int upperbound = 1000;
+            Random rand = new Random();
+            int int_random = rand.nextInt(upperbound);
             System.out.println("x before assigning new Value :  "+int_random+"");
             String randomUsername = "PaulGeorge" + int_random;
             String randomEmail = "PaulGeorge" + int_random + "@cooper.edu";
@@ -32,14 +33,7 @@ public class App {
                     + "VALUES ("+int_random+",'Paul', 'Kim', '"+randomUsername+"', 'abc123', '"+randomEmail+"');";
             statement.executeUpdate(insertSQL);
 
-            // READ:
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM accounts");
-
-            while(resultSet.next()){
-                System.out.println(resultSet.getInt(1)); // count rows
-            }
-
-            // UPDATE: Username
+            // UPDATE: Update Username
             String updateSQL = "UPDATE accounts "
                     + "SET first_name = ? "
                     + "WHERE user_id = ?";
@@ -48,6 +42,13 @@ public class App {
             pstmt.setString(1, "Bobby");
             pstmt.setInt(2, 101);
             pstmt.executeUpdate();
+
+            // READ:
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM accounts");
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getInt(1)); // count rows
+            }
 
             // Close the connections
             statement.close();
